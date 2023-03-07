05156e65c57de8e894882c47ccf258bdcb0fc684
SalamsettyNandiniDevi <124499441+SalamsettyNandiniDevi@users.noreply.github.com>
2023-03-07 11:51:32 +0530

    Nandini

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import N from './N'
+
+const App = () => {
+  return (
+    <div>
+      HE
+      
+      <N/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/N.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const N = () => {
+  return (
+    
+    <div className='a'>
+        
+      <div class="btn-group">
+        
+  <button class="button">Button</button>
+  
+  </div>
+      <button>click Me</button>
+      
+<div>
+    
+</div>
+    </div>
+  )
+}
+
+export default N
